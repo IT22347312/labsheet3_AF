6609bbd172d4a55861226d3106676b66f2c63640
HirushiniChandrasoma <it22347312@my.sliit.lk>
2025-03-02 11:50:06 +0530

    include promises

--- app.js
+++ app.js
@@ -57,4 +57,13 @@ const myPromise = new Promise((resolve, reject) => {
     console.log(error);
    });
 
-   
+   //Async/Await
+   async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+   }
+   myFunction();
