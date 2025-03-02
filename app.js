48b17e7f53b78d3770b3a1fdc3411c180063d431
HirushiniChandrasoma <it22347312@my.sliit.lk>
2025-03-02 11:49:37 +0530

    Async/Await

--- app.js
+++ app.js
@@ -40,6 +40,21 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 const myModule = require('./my-module.js');
 console.log(myModule.myFunction());
 
-);
 
 
+
+//Promises
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+    resolve('Success!');
+    } else {
+    reject('Failure!');
+    }
+   });
+   myPromise.then((result) => {
+    console.log(result);
+   }).catch((error) => {
+    console.log(error);
+   });
+
+   
