80062dffc8235a4917a3ff46d211a28f7ab8d4d8
HirushiniChandrasoma <it22347312@my.sliit.lk>
2025-03-02 11:46:12 +0530

    include read file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hello, this is a to check if working!");
+
+//read file
+import { readFile } from 'fs';
+readFile('file.txt', 'utf8', function (err, data) {
+ if (err) throw err;
+ console.log(data);
+});
+
